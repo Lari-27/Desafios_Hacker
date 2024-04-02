68f762c12b058c590c082c5df34af3e4fbaa297b
Larissa Nogueira <128263770+Lari-27@users.noreply.github.com>
2024-04-02 16:58:18 -0300

    Update InsertionSort1.rb

--- InsertionSort1.rb
+++ InsertionSort1.rb
@@ -12,7 +12,7 @@ require 'stringio'
 #
 
 #explicação: 
-# Inserir elemento na lista ordenada
+# Inserir número na lista ordenada
 # Dada uma lista ordenada, com um número não ordenadona na célula mais à direita,
 # você pode escrever algum código simples para inserir na
 # matriz para que ela permaneça classificada?
@@ -24,25 +24,17 @@ require 'stringio'
 #i é usado para percorrer a lista enquanto estamos inserindo o valor na posição correta
 
 
-def insertionSort1(n, arr) #'n - tamanho da lista' 'arr- lista a ser classificada'
-# Write your code here
-  #Armazena o valor do último número da lista não ordenada
+def insertionSort1(n, arr) 
   value = arr[n - 1] 
-  #começa a comparação do segundo último número
   i = n - 2 
-  #loop enquanto ainda houver número à esquerda, e o valor do número atual for maior do que o valor armazenado
   while i >= 0 && arr[i] > value
-  #mover o número atual para direita
     arr[i + 1] = arr[i] 
-      puts arr.join(' ') #imprime a lista atualizada. (todos os elementos do array em uma unica string)
-      i -= 1 #move o próximo número para esquerda 
+      i -= 1
   end
-  arr[i + 1] = value  #insere o valor armazenado na posição correta
-    puts arr.join(' ') #imprimi a lista final.
+  arr[i + 1] = value
+    puts arr.join(' ') 
 end
 
-n = gets.strip.to_i
-
-arr = gets.rstrip.split.map(&:to_i)
-
-insertionSort1 n, arr
+arr = [1, 3, 4, 5, 2]
+n = 5
+insertionSort1(n, arr)
